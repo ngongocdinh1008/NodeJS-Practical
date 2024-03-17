00f593b282d0dccd9624c28da39942909d5aa1ee
JRosé <89382734+ngongocdinh1008@users.noreply.github.com>
2024-03-17 17:36:52 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,43 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const Product = require('./models/product');
 
 const app = express();
 
-app.use(bodyParser.json());
-
-mongoose.connect('mongodb://localhost:27017/product_management', {
+mongoose.connect('mongodb://localhost:27017/product-management', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
-
-const ProductSchema = new mongoose.Schema({
-  ProductCode: String,
-  ProductName: String,
-  ProductDate: Date,
-  ProductOriginPrice: Number,
-  Quantity: Number,
-  ProductStoreCode: String
-});
-
-const Product = mongoose.model('Product', ProductSchema);
 
 app.get('/products', async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
+  const products = await Product.find().sort({ productStoreCode: -1 });
+  res.send(products);
 });
 
 app.post('/products', async (req, res) => {
   const product = new Product(req.body);
   await product.save();
-  res.json(product);
+  res.send(product);
 });
 
 app.delete('/products/:id', async (req, res) => {
   await Product.findByIdAndDelete(req.params.id);
-  res.json({ message: 'Product deleted successfully' });
+  res.send('Product deleted successfully');
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log('Server is running on port 3000');
 });
